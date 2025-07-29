cb6373d76ac9db12b96df441a4af7a18aa9bb53a
sahilthorat3132 <sahilthorat664@gmail.com>
2025-07-29 16:01:27 +0530

    01-variable js
    01-variable js

---
+++ 01-variable.js
@@ -0,0 +1,16 @@
+var city="Pune";
+console.log("My city name is : "+ city);
+
+var name="Rahul Gandhi";
+console.log("My best friend name is : "+ name);
+
+var country="India";
+console.log("My country name is : "+ country);
+
+
+var school="Army Public School";
+console.log("my school name is : " + school);
+
+var clg="MIT WPU University "
+console.log("my college name is : "+ clg);
+
---
+++ 02-varible.js
@@ -0,0 +1,35 @@
+var name
+console.log("my full name is : " +name);
+
+
+name="Sahil Thorat"
+console.log("my full name is : " +name);
+
+
+name="Sahil Satish Thorat"
+console.log("my full name is : " +name);
+
+
+console.log("===================================================");
+
+
+myLove="I love only JS";
+console.log("I love only : " +myLove);
+
+firstName="Sahil";
+console.log("my first name is : " +firstName);
+
+lastName="Thorat"
+console.log("my last name is : " +lastName);
+
+var number=24;
+console.log("my age is : " +number);
+
+number=411024;
+console.log("pincode is : " +number);
+
+var gender="Male";
+console.log("Gender : " +gender);
+
+var isMarried="Single";
+console.log("Married Status : " +isMarried);
